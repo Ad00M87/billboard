4b3f90daaef45c39f90f185c8adeb49172022378
Adam Kimball <ad00m@Adams-MacBook-Pro.local>
2017-09-01 09:29:20 -0600

    finished styling with some defects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 40.times do
+#   Artist.create(name: Faker::Name.name_with_middle,
+#                 from: Faker::LordOfTheRings.location,
+#                 music_genre: Faker::Music.instrument,
+#                 age: Faker::Number.between(18, 100))
+# end
+
+# Artist.all.each do |artist|
+#   Song.create(name: Faker::Beer.name,
+#               artist_id: artist.id,
+#               genre: Faker::Space.moon)
+# end
+
+10.times do
+  BillboardChart.create(
+    name: "Top 100 #{Faker::University.name}")
+  end
+
+Song.all.each do |song|
+  BillboardChart.all.each do |chart|
+    song.find(params[song]).billboard_chart_id = chart.find(params[chart])
